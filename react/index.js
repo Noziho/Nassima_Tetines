87c0f2abea8e99866410905e09f9920a6a7831c4
Noziho <noah.decroix3@gmail.com>
2023-05-16 14:52:20 +0200

    add register and login page

--- react/index.js
+++ react/index.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {Home} from './pages/Home';
+import {Register} from "./pages/user/Register";
+import {Login} from "./pages/user/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -7,6 +9,8 @@ root.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
         </Routes>
     </BrowserRouter>
 );
---
+++ react/pages/user/Register.jsx
@@ -0,0 +1,10 @@
+export const Register = () => {
+    return (
+        <>
+            <h1>Register</h1>
+            <form action="">
+                <input type="text"/>
+            </form>
+        </>
+    )
+}
