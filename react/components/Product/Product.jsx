6929160712e5ccd235ae9001466167942cf6043f
Noziho <noah.decroix3@gmail.com>
2023-05-30 15:15:04 +0200

    Products styles

--- react/components/Product/Product.jsx
+++ react/components/Product/Product.jsx
@@ -3,8 +3,18 @@ import './Product.scss';
 export const Product = ({product}) => {
     return (
         <>
-            <div>
-                <h1>{product.name}</h1>
+            <div className={"product"}>
+                <div className={"productPresentation"}>
+                    <img className={"productImg"} src={"/img/productsImg/" + product.image} alt="Image"/>
+                    <h1 className={"productName"}>{product.name}</h1>
+                </div>
+
+                <div className={"persoButton"}>
+                    <button>
+                        Personnalisez
+                    </button>
+                </div>
+
             </div>
         </>
 
